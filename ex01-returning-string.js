62f1e85df82f04baa91b2ffa46fc9c15b00b7f01
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-07-22 19:59:44 -0300

    .

--- ex01-returning-string.js
+++
@@ -1,9 +0,0 @@
-//Crie uma função que aceite um parâmetro representando um nome e retorne a mensagem: "Olá, <nome>, como vai você hoje?".
-
-function greet(name){
-    return `Hello, ${name} how are you doing today?`
-}
-
-
-let n = 'Ygor'
-console.log(greet(n))
--- ex018.js
+++
@@ -1,9 +0,0 @@
-/*
-Escreva uma função que converta a string de entrada para letras maiúsculas.*/
-
-function makeUpperCase(str) {
-    return str.toUpperCase()
-}
-
-console.log(makeUpperCase("hello!"))
-console.log(makeUpperCase("Good morning!"))
